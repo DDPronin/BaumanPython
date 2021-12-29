c7dd261ff92cf9e0b8830ede5dd7fe7af2d1c1ad
Dmitry <DDPronin003@yandex.ru>
2021-12-29 20:10:04 +0300

    #1 Commit

--- Reg.py
+++ Reg.py
@@ -2,10 +2,20 @@ import re
 
 input_string = "АНАЛЬГИН 5 МГ"
 
+with open('лекстроки.txt', 'r', encoding='utf-8') as dataFile:
+    data = []
+    for line in dataFile:
+        data.append(line.rstrip('\n'))
+
 
 def main(string):
-    value = re.findall(r'\d\s\w+', string)
+    # value = re.findall(r'\d+[МГ]\w+', string)
+    value = re.findall(r'\d+\s?МГ|\d+\s?МЛ|\d+\s?Г', string)  # для единиц массы и объема в СИ
     return value
 
 
-print(main(input_string))
+num = 0
+for element in data:
+    num += 1
+    print(num, main(element))
+
